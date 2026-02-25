b36b9a3310c8b9b79f3a400e096f78726869f8c4
alexsanchezsa <sanchez.peale23@zaragoza.salesianos.edu>
2026-02-25 17:36:21 +0100

    style: reformat automático de Eclipse en Habilidad.java

--- eclipse-workspace/ExamenSanchezPeraltaAlejandroDI/src/main/java/com/examen/sanchezperalta/entity/Habilidad.java
+++ eclipse-workspace/ExamenSanchezPeraltaAlejandroDI/src/main/java/com/examen/sanchezperalta/entity/Habilidad.java
@@ -31,8 +31,11 @@ public class Habilidad {
 
     public Habilidad() {}
 
-    public Long getId()                 { return id; }
-    public void setId(Long id)          { this.id = id; }
+    public Long getId()                
+    { return id; }
+    public void setId(Long id){
+    	this.id = id; 
+    }
 
     public String getNombre()               { return nombre; }
     public void setNombre(String nombre)    { this.nombre = nombre; }
